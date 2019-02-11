dc1ef7ab7c2546ff51cbf09a6096582653d7eeba
Chamindu <chaminduilshan@gmail.com>
2019-02-11 12:04:40 +0530

    finish

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,18 +1,41 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular'
-import {LionsInternationalHomePage} from '../lions-international-home/lions-international-home'
-import{District306a1homePage}from'../district306a1home/district306a1home'
-import{AboutLionismPage}from'../about-lionism/about-lionism'
+import { LionsInternationalHomePage } from '../lions-international-home/lions-international-home'
+import { District306a1homePage } from '../district306a1home/district306a1home'
+import { AboutLionismPage } from '../about-lionism/about-lionism'
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AngularFireDatabase, AngularFireAction } from '@angular/fire/database'
+import { Observable } from 'rxjs/observable';
+import { FirebaseDatabase } from '@angular/fire';
+import { FirebaseListObservable } from '@angular/fire/database-deprecated'
+import { filter, map } from 'rxjs/operators';
+import { ViewChild } from '@angular/core';
+import { Navbar, Searchbar, LoadingController } from 'ionic-angular';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  items: Observable<AngularFireAction<firebase.database.DataSnapshot>[]>;
+  tempItems: Observable<AngularFireAction<firebase.database.DataSnapshot>[]>;
+  key;
+  mdb: AngularFireDatabase;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public db: AngularFireDatabase) {
+    this.mdb = db;
+    this.init();
   }
 
+  init() {
+    this.items = this.mdb.list('Notifications').snapshotChanges();
+    this.tempItems = this.items;
+    this.tempItems.subscribe(i => {
+      console.log("Working");
+      console.log(i);
+    });
+    console.log("ITEMS:");
+    console.log(this.items instanceof Observable);
+  }
   goToLionsInternational() {
     this.navCtrl.push(LionsInternationalHomePage)
   }
@@ -25,10 +48,10 @@ export class HomePage {
   tapDistrict(para: any) {
     this.goToLionsDistrict306a1();
   }
-  tapLionsm(para: any){
+  tapLionsm(para: any) {
     this.goToLionism();
   }
-  goToLionism(){
+  goToLionism() {
     this.navCtrl.push(AboutLionismPage);
   }
 
